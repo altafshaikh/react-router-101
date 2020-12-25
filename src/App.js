afbb62b5fe447b2b0b028ea25a43e784f12c5111
altafshaikh <iamaltafshaikh07@gmail.com>
2020-12-25 11:10:15 +0530

    added components-nav,home,shop,about,contact

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Shop from "./components/Shop";
+import ContactUs from "./components/ContactUs";
+import About from "./components/About";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Nav />
+        <Home />
+        <Shop />
+        <ContactUs />
+        <About />
       </header>
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div>
+      <nav>
+        <div>
+          <h2>Company Logo</h2>
+        </div>
+        <div>
+          <ul>
+            <li>About us</li>
+            <li>Contatc Us </li>
+            <li>Shop</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
