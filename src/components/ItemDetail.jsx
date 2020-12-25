017657bcabe33f8391eca70006a7aa4849e83e2f
altafshaikh <iamaltafshaikh07@gmail.com>
2020-12-25 12:03:04 +0530

    fetching detail of shop item and displaying details of specific item dynamically

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function ItemDetail() {
-  return (
-    <div>
-      <h1>item detail</h1>
-    </div>
-  );
+export default class ItemDetail extends Component {
+  constructor() {
+    super();
+    this.state = {
+      item: {},
+    };
+  }
+  componentDidMount = () => {
+    fetch(
+      `https://fortnite-api.theapinetwork.com/item/get?id=${this.props.match.params.id}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ item: data.data.item });
+      })
+      .catch((err) => {
+        console.log(err);
+        return err;
+      });
+  };
+  render() {
+    return (
+      <div>
+        <h1>item detail</h1>
+        <p>Item Name: {this.state.item.name}</p>
+        <p>Description: {this.state.item.description}</p>
+      </div>
+    );
+  }
 }
