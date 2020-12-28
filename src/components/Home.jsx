4ee65700b360bb279e6cd7090dc84d7ba101e1cf
altafshaikh <iamaltafshaikh07@gmail.com>
2020-12-28 14:22:08 +0530

    used history object to push to different url

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+export default class Home extends Component {
+  navigatePage = () => {
+    this.props.history.push("/about");
+  };
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <button onClick={this.navigatePage}>Goto About Us</button>
+      </div>
+    );
+  }
 }
